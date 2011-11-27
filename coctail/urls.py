a3643c8116adac369ba785279379765def1caabf
Pokutnik Aleksandr <pokutnik@gmail.com>
2011-11-27 20:10:49 +0200

    added index page

--- coctail/urls.py
+++ coctail/urls.py
@@ -1,17 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'coctail.views.home', name='home'),
-    # url(r'^coctail/', include('coctail.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^$', TemplateView.as_view(template_name="index.html")),
 )
